296e89f71274d7d090df9dd4c71a4607daa095e7
manikandandhanabalan <manikandandhanabalan@macpro-r-13.hsd1.ga.comcast.net>
2018-12-20 21:26:42 -0500

    Inital Push

---
+++ src/main/java/com/codebuild/demo/ApplicationController.java
@@ -0,0 +1,22 @@
+package com.codebuild.demo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+
+    @RequestMapping(value="/", method = RequestMethod.GET, produces ="application/hal+json")
+    public String sayHello() {
+    	String response = "Code Build Test 1220 0810";
+        return response;
+    }
+
+    @RequestMapping(value="/hello/{name}", method = RequestMethod.GET, produces ="application/hal+json")
+    public String sayHelloName(@PathVariable(value="name") String name) {   	
+    	String response = "Hello " + name;
+        return response;
+    }
+}
